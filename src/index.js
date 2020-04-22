b288f900c64bb37210cb6b1504c19ffe6f2e83e9
xadparo <45482148+xadparo@users.noreply.github.com>
2020-04-22 17:41:31 +0900

    parse 기능 추가 및 main file 지정

---
+++ src/index.js
@@ -0,0 +1,57 @@
+function extractPathByExpression(expression) {
+  var [, $1] = expression.match(/\{(.{0,})\}/)
+  return $1
+}
+
+function findTargetByPath($, path) {
+  return path.split('.').reduce((point, key) => {
+    if(key === '$')
+      return $
+
+    if (Array.isArray(point)) {
+      return point.map(sub => sub[key])
+    } else {
+      return point[key]
+    }
+  }, null)
+}
+
+function findTargetByExpression($, expression) {
+  return findTargetByPath($, extractPathByExpression(expression))
+}
+
+function getLinksByObject(obj) {
+  return obj._links || (obj._links = {})
+}
+function bindLinkToObject(obj, name, link) {
+  var links = getLinksByObject(obj)
+  links[name] = link
+}
+function convertExpression(expression, value) {
+  return expression.replace(/(\{.{0,}\})/, value)
+}
+
+function parse($) {
+  var { _links } = $
+
+  for(var name in _links) {
+    var _link = _links[name]
+      , { template, rel, href, method } = _link
+
+    if(template) {
+      var targets = findTargetByExpression($, template)
+        , rels = findTargetByExpression($, rel)
+        , hrefs = findTargetByExpression($, href)
+
+      targets.forEach((target, idx) => {
+        bindLinkToObject(target, name, {
+          rel: convertExpression(rel, rels[idx]),
+          href: convertExpression(href, hrefs[idx]),
+          method,
+        })
+      })
+    }
+  }
+}
+
+module.exports = parse
